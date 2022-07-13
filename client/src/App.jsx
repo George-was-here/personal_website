9d38c4a5b2158430120c583bcb9fd8506779e2e3
George-was-here <george.burt@gmail.com>
2022-07-13 03:45:42 +0000

    Updating App.jsx skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main'
+
 import './App.scss';
 
 function App() {
   
   return (
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        <Footer />
+      </div>
+    </main>
   );
 };
 
