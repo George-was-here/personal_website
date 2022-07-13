c5116b1055827950083ec5e6a4f21446221d47ae
George-was-here <george.burt@gmail.com>
2022-07-13 04:30:47 +0000

    Debugging and removing unneccesarry code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,26 @@ import { Route, Routes } from 'react-router-dom';
 
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-import Main from './components/Main/Main'
+// import Main from './components/Main/Main'
 
 import './App.scss';
 
 function App() {
+  console.log('heyyyy')
+
+  return (
+    <h1>heyyyyyyy</h1>
+  );
   
   return (
 
     <main>
+        <h1>hey bbbbbb</h1>
       <div className="App">
+      
         <Navbar />
           <Routes>
-            <Route path="/" element={<Main />} />
+            {/* <Route path="/" element={<Main />} /> */}
           </Routes>
         <Footer />
       </div>
